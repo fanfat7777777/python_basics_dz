c9c04ad7a3ad411d19d4400883f5fef0a6ab6e38
thor7777777 <thor7777777@mail.ru>
2021-12-03 10:32:03 +0300

    Упрощение кода на основе увиденного ДЗ

--- Levanov_Ilya_dz_1/Levanov_Ilya_dz_1_1/main.py
+++ Levanov_Ilya_dz_1/Levanov_Ilya_dz_1_1/main.py
@@ -1,21 +1,11 @@
-duration = 400153
+duration = 3600
 
-if duration < 60:
-    print(duration, 'сек')
-elif duration < 3600:
-    t_min = duration // 60
-    t_sec = duration - 60 * t_min
-    print(f'{t_min} мин {t_sec} сек')
-elif duration < 86400:
-    t_hour = duration // 3600
-    sec = duration - 3600 * t_hour
-    t_min = sec // 60
-    t_sec = sec - t_min * 60
-    print(f'{t_hour} ч {t_min} мин {t_sec} сек')
-else:
-    t_day = duration // 86400
-    t_hour = (duration - t_day * 86400) // 3600
-    sec = duration - t_day * 86400 - t_hour * 3600
-    t_min = sec // 60
-    t_sec = sec - t_min * 60
-    print(f'{t_day} дн {t_hour} ч {t_min} мин {t_sec} сек')
+t_day = duration // 86400
+t_hour = (duration - t_day * 86400) // 3600
+sec = duration - t_day * 86400 - t_hour * 3600
+t_min = sec // 60
+t_sec = sec - t_min * 60
+
+print(f'{t_day} дн {t_hour} ч {t_min} мин {t_sec} сек')
+
+#Упрощение кода на основании увиденного в выполненных дз
