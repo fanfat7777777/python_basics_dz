01075796e27269b6577a1dd55c8d0e5a22b1392e
thor7777777 <thor7777777@mail.ru>
2021-12-03 11:20:37 +0300

    То к чему стоит стремиться

--- Levanov_Ilya_dz_1/Levanov_Ilya_dz_1_2/main.py
+++ Levanov_Ilya_dz_1/Levanov_Ilya_dz_1_2/main.py
@@ -1,25 +1,43 @@
-first_list = []
-
-for i in range(1, 1001, 2):
-    first_list.append(i ** 3)
-
-print(first_list)
-
-
-def sum_num(additional_number):
-    sum_number_list = 0
-    for num in first_list:
-        number = num + additional_number
-        sum_number = 0
-        while number != 0:
-            sum_number += number % 10
-            number //= 10
-
-        if sum_number % 7 == 0:
-            sum_number_list += num
-    print('a)', sum_number_list)
-
-sum_num(0)
-sum_num(17)
+# first_list = []
+#
+# for i in range(1, 1001, 2):
+#     first_list.append(i ** 3)
+#
+# print(first_list)
+#
+#
+# def sum_num(additional_number):
+#     sum_number_list = 0
+#     for num in first_list:
+#         number = num + additional_number
+#         sum_number = 0
+#         while number != 0:
+#             sum_number += number % 10
+#             number //= 10
+#
+#         if sum_number % 7 == 0:
+#             sum_number_list += num
+#     print('a)', sum_number_list)
+#
+# sum_num(0)
+# sum_num(17)
 
 #упрощение после просмотра выполненных ДЗ
+
+#То к чему стоит стремиться:
+def sum_numbers(value):
+    result = 0
+
+    while value != 0:
+        result += value % 10
+        value //= 10
+
+    return result
+
+cub_my = [i**3 for i in range(1, 1001, 2)]
+
+result_a = sum(filter(lambda num: sum_numbers(num) % 7 == 0, cub_my))
+result_b = sum(filter(lambda num: sum_numbers(num + 17) % 7 == 0, cub_my))
+
+print(result_a)
+print(result_b)
