9b6d03b02874fd9677045fc76276d22dcb1a40c3
thor7777777 <thor7777777@mail.ru>
2021-12-03 11:00:39 +0300

    упрощение после просмотра выполненных ДЗ
    Выполнение в функции оставил

--- Levanov_Ilya_dz_1/Levanov_Ilya_dz_1_2/main.py
+++ Levanov_Ilya_dz_1/Levanov_Ilya_dz_1_2/main.py
@@ -1,28 +1,25 @@
 first_list = []
 
-for i in range(1, 1001):
-    if i % 2 != 0:
-        number = i**3
-        first_list.append(number)
+for i in range(1, 1001, 2):
+    first_list.append(i ** 3)
 
 print(first_list)
 
 
 def sum_num(additional_number):
     sum_number_list = 0
-    i = 0
-    while i < len(first_list):
-        number = first_list[i] + additional_number
+    for num in first_list:
+        number = num + additional_number
         sum_number = 0
         while number != 0:
             sum_number += number % 10
             number //= 10
 
         if sum_number % 7 == 0:
-            sum_number_list += first_list[i]
-        i += 1
-
+            sum_number_list += num
     print('a)', sum_number_list)
 
 sum_num(0)
 sum_num(17)
+
+#упрощение после просмотра выполненных ДЗ
