d539ec4c158f7f439e83555b621d6e3c9f8b0e08
thor7777777 <thor7777777@mail.ru>
2021-12-08 13:52:22 +0300

    update files 4_4

--- Levanov_Ilya_dz_4/Levanov_Ilya_dz_4_4/utils.py
+++ Levanov_Ilya_dz_4/Levanov_Ilya_dz_4_4/utils.py
@@ -1,21 +1,23 @@
 from requests import get, utils
+from decimal import Decimal
 
-response = get('http://www.cbr.ru/scripts/XML_daily.asp')
-
-encodings = utils.get_encoding_from_headers(response.headers)
-content = response.content.decode(encoding=encodings)
-# print(response.headers['Date'])
-
-currency_dict = {}
-
-for v in content.split('</Value>'):
-    i = v.split('</CharCode>')[0][-3:]
-    if i.isalpha():
-        currency_dict[i] = v[-7:]
 
 def currency_rates(char_code):
-    print('Неверный код валюты') if currency_dict.setdefault(char_code) == None \
-        else print(f'{char_code} = {currency_dict[char_code]} руб.')
+    response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+    encodings = utils.get_encoding_from_headers(response.headers)
+    content = response.content.decode(encoding=encodings)
+
+    currency_dict = {}
+    for n in content.split('</Value>'):  # Находим код валюты и её значения
+        i = n.split('</CharCode>')[0][-3:]
+        currency_dict[i] = n[-7:] if i.isalpha() else print()  # Создаём список с валютой и её стоимостью
+
+    char_code = char_code.upper()
+    if currency_dict.setdefault(char_code) == None:
+        print('Неверный код валюты')
+    else:  # Если ключ есть в списке выдаём значение валюты
+        price = Decimal(currency_dict[char_code].replace(',', '.')).quantize(Decimal('0.01'))
+        print(f'{char_code} = {price} руб.')
 
 if __name__ == "__main__":
     currency_rates(input('Введите код валюты: '))
