1c24d179a9bc54af716bb2c792759a07213dfcd1
thor7777777 <thor7777777@mail.ru>
2021-12-17 19:50:39 +0300

    dz_7_2 создание цикла

--- Levanov_Ilya_dz_7/Levanov_Ilya_dz_7_2/main.py
+++ Levanov_Ilya_dz_7/Levanov_Ilya_dz_7_2/main.py
@@ -1,32 +1,31 @@
 import os
 
-# то что должно получиться
-test = {
-    'my_project': {
-        'settings': [
-            '__init__.py',
-            'dev.py',
-            'prod.py'],
-        'mainapp': [
-            '__init__.py',
-            'models.py',
-            'views.py', {
-                'templates': {
-                    'mainapp': [
-                        'base.html',
-                        'index.html'
-                    ]
-                }
-            }
-        ]
-    }
-}
-
 dir_project = {}
-my_project = os.getcwd()
+
 
 with open('config.yaml', 'r') as conf:
     line = conf.readlines()
-    for i in range(len(line)):          # Перебираем по строкам
-        print(line[i])
-        
+
+    for i in range(1, len(line)):                                       # Перебираем по строкам
+        l_new = line[i].strip().replace('|--', '').replace('|', '')     # Наша текущая строка
+        l_old = line[i-1].strip().replace('|--', '').replace('|', '')   # Предыдущая строка
+
+        # Если файла нет, то
+        if not os.path.exists(l_new):
+            # Создаём каталог
+            if not l_new.count(' '):
+                print()
+                my_project = os.getcwd() + rf'\{l_new}'
+                print(my_project)
+            else:
+                # Если пробелы есть
+                # Проверяем является ли это именем файла
+                if l_new.count('.'):
+                    print(my_project + '\\' + l_new.strip())
+                else:
+                    # Если не файл, то это директория
+                    if l_new.count(' ') > l_old.count(' '):     # Новый подкаталог
+                        my_project += '\\' + l_new.strip()
+                        print(my_project)
+                    elif l_new.count(' ') == l_old.count(' '):  # Папка в каталоге
+                        print(my_project + '\\' + l_new.strip())
