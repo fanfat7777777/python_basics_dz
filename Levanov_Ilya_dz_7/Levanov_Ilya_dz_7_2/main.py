bcd88fc33488abad95cbb90ce836a7b23fc83127
thor7777777 <thor7777777@mail.ru>
2021-12-18 11:17:26 +0300

    dz_7_2 completed

--- Levanov_Ilya_dz_7/Levanov_Ilya_dz_7_2/main.py
+++ Levanov_Ilya_dz_7/Levanov_Ilya_dz_7_2/main.py
@@ -1,31 +1,36 @@
 import os
 
 dir_project = {}
+my_way = os.getcwd()
+
+
+def secyr(way, key_way, name):
+    # Если файла нет, то создаём его
+    if not os.path.exists(name):
+        os.mkdir(name)
+    # Если это ключевая директория, то переходим в неё
+    os.chdir(way + rf'\{name}') if key_way else None
 
 
 with open('config.yaml', 'r') as conf:
     line = conf.readlines()
+    # Перебираем по строкам
+    for i in range(1, len(line)):
+        l_new = line[i].strip().replace('|--', '').replace('|', '')         # Наша текущая строка
+        l_old = line[i - 1].strip().replace('|--', '').replace('|', '')     # Предыдущая строка
 
-    for i in range(1, len(line)):                                       # Перебираем по строкам
-        l_new = line[i].strip().replace('|--', '').replace('|', '')     # Наша текущая строка
-        l_old = line[i-1].strip().replace('|--', '').replace('|', '')   # Предыдущая строка
-
-        # Если файла нет, то
-        if not os.path.exists(l_new):
-            # Создаём каталог
-            if not l_new.count(' '):
-                print()
-                my_project = os.getcwd() + rf'\{l_new}'
-                print(my_project)
+        if not l_new.count(' '):        # Если каталог находится в корне проекта
+            os.chdir(my_way)            # Возвращаемся в корень
+            secyr(my_way, 1, l_new.strip())
+        else:
+            # Если пробелы есть
+            # Проверяем является ли это именем файла
+            if l_new.count('.'):
+                with open(f'{l_new.strip()}', 'w'):
+                    pass
             else:
-                # Если пробелы есть
-                # Проверяем является ли это именем файла
-                if l_new.count('.'):
-                    print(my_project + '\\' + l_new.strip())
-                else:
-                    # Если не файл, то это директория
-                    if l_new.count(' ') > l_old.count(' '):     # Новый подкаталог
-                        my_project += '\\' + l_new.strip()
-                        print(my_project)
-                    elif l_new.count(' ') == l_old.count(' '):  # Папка в каталоге
-                        print(my_project + '\\' + l_new.strip())
+                # Если это директория
+                if l_new.count(' ') > l_old.count(' '):  # Новый подкаталог (Ключевой путь)
+                    secyr(os.getcwd(), 1, l_new.strip())
+                elif l_new.count(' ') == l_old.count(' '):  # Папка в каталоге
+                    secyr(os.getcwd(), 0, l_new.strip())
