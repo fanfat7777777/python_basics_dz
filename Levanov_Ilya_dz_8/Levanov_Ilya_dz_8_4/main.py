54d583766dd0bbad1516cfac7f3d3ce804bb3615
thor7777777 <thor7777777@mail.ru>
2021-12-22 07:20:30 +0300

    dz_8_4 update

--- Levanov_Ilya_dz_8/Levanov_Ilya_dz_8_4/main.py
+++ Levanov_Ilya_dz_8/Levanov_Ilya_dz_8_4/main.py
@@ -1,7 +1,14 @@
 def arg_dec(arg_decoratora):
     def my_decorator(function):
         def wrapper(arg):
-            return function(arg) if arg_decoratora(arg) else f'Неверный аргумент: {arg}'
+            try:
+                return function(arg) if arg_decoratora(arg) else f'Неверный аргумент: {arg}'
+            except NameError as err:
+                return err
+            except TypeError as err:
+                return err
+            except:
+                return 'Неизвестная ошибка'
         return wrapper
     return my_decorator
 
@@ -11,4 +18,4 @@ def calc_cube(x):
     return x ** 3
 
 
-print(calc_cube(3))
+print(calc_cube('1.2'))
