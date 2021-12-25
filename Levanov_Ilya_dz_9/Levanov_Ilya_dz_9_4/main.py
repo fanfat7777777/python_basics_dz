d56be38e29798ee2bbe289667b616e1359e1e72c
thor7777777 <thor7777777@mail.ru>
2021-12-25 10:54:55 +0300

    9_4 create files

---
+++ Levanov_Ilya_dz_9/Levanov_Ilya_dz_9_4/main.py
@@ -0,0 +1,35 @@
+class Car:
+    speed = ''
+    color = ''
+    name = ''
+    is_police = False
+
+    def go(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def turn(direction):
+        pass
+
+    def show_speed(self):
+        pass
+
+
+class TownCar(Car):
+    pass
+
+
+class SportCar(Car):
+    pass
+
+
+class WorkCar(Car):
+    pass
+
+
+class PoliceCar(Car):
+    pass
+
+print(type(Car.turn(1)))
