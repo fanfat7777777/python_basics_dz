b6f620906d3d5b1ab89d541f402a5752dbdd659f
thor7777777 <thor7777777@mail.ru>
2021-12-26 20:56:18 +0300

    dz_9_4 completed

--- Levanov_Ilya_dz_9/Levanov_Ilya_dz_9_4/main.py
+++ Levanov_Ilya_dz_9/Levanov_Ilya_dz_9_4/main.py
@@ -1,24 +1,41 @@
 class Car:
-    speed = ''
-    color = ''
-    name = ''
+    speed = 0
+    color = 'white'
+    name = 'Nissan'
     is_police = False
 
     def go(self):
-        pass
+        return f'Машина {self.__class__.__name__} поехала'
 
     def stop(self):
-        pass
+        return f'Машина {self.__class__.__name__} остановилась'
 
-    def turn(direction):
-        pass
+    def turn(self, direction):
+        self.direction = direction
+        return f'Машина {self.__class__.__name__} повернула на {self.direction}'
 
-    def show_speed(self):
-        pass
+    def show_speed(self, speed):
+        self.speed = speed
+        return f'Скорость машины {self.__class__.__name__} = {self.speed}'
+
+    # def check_speed(self, speed):                 # вариант проверки
+    #     self.speed = speed
+    #     if self.__class__.__name__ == 'TownCar' and self.speed > 60:
+    #         return f'{self.__class__.__name__} Вы превысили скорость'
+    #     elif self.__class__.__name__ == 'WorkCar' and self.speed > 40:
+    #         return f'Машина {self.__class__.__name__} превысила скорость'
+    #     else:
+    #         return f'Скорость машины {self.__class__.__name__} = {self.speed}'
 
 
 class TownCar(Car):
-    pass
+    def show_speed(self, speed):
+        self.speed = speed
+        # return self.check_speed(self.speed)       # вариант проверки
+        if self.speed > 60:
+            return f'{self.__class__.__name__} Вы превысили скорость'
+        else:
+            return f'Скорость машины {self.__class__.__name__} = {self.speed}'
 
 
 class SportCar(Car):
@@ -26,10 +43,38 @@ class SportCar(Car):
 
 
 class WorkCar(Car):
-    pass
+    def show_speed(self, speed):
+        self.speed = speed
+        # return self.check_speed(self.speed)       # вариант проверки
+        if self.speed > 40:
+            return f'Машина {self.__class__.__name__} превысила скорость'
+        else:
+            return f'Скорость машины {self.__class__.__name__} = {self.speed}'
 
 
 class PoliceCar(Car):
     pass
 
-print(type(Car.turn(1)))
+b = TownCar()
+print(b.go())
+print(b.show_speed(50))
+print(b.turn('лево'))
+print(b.show_speed(70))
+print(b.stop())
+print('#'*50)
+
+w = WorkCar()
+print(w.go())
+print(w.show_speed(20))
+print(w.turn('право'))
+print(w.show_speed(45))
+print(w.stop())
+print('#'*50)
+
+p = PoliceCar()
+print(p.go())
+print(p.show_speed(60))
+print(p.turn('лево'))
+print(p.show_speed(80))
+print(p.stop())
+print('#'*50)
