9ead6b4dd3fd7e8ba9163665e7227b0e1b553447
thor7777777 <thor7777777@mail.ru>
2021-12-30 12:18:34 +0300

    dz_10_2 update

--- Levanov_Ilya_dz_10/Levanov_Ilya_dz_10_2/main.py
+++ Levanov_Ilya_dz_10/Levanov_Ilya_dz_10_2/main.py
@@ -5,23 +5,25 @@ class Person:
         self.__meaning = meaning
         self.__len_cloth = 0
 
-    def get_cloth(self):
-        return f'{self.__name}: {self.__len_cloth}'
+    @property
+    def cloth(self):
 
-    def set_cloth(self, meaning):
+        return f'{self.__name}: {self.__len_cloth}' if self.__len_cloth != 0 \
+            else 'Введены не верные значения'
+
+    @cloth.setter
+    def cloth(self, meaning):
         self.__meaning = meaning
         if self.__name == 'пальто':
             if self.__meaning < 75:
-                self.__len_cloth = self.__meaning / 6.5 + 0.5
+                self.__len_cloth = round(self.__meaning / 6.5 + 0.5, 2)
         elif self.__name == 'костюм':
             if self.__meaning < 200:
-                self.__len_cloth = self.__meaning * 2 + 0.3
-        else:
-            raise AttributeError('Введены не верные значения')
-
-    inf = property(get_cloth, set_cloth)
+                self.__len_cloth = round(self.__meaning * 2 + 0.3, 2)
 
 
 p = Person('Пальто', 20)
-p.inf = 65
-print(p.inf)
+p.cloth = 55
+print(p.cloth)
+
+
